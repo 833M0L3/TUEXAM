6d1fb5419e2f1690abe0c98f7abee77718119394
Pi <bimaldhital@gmail.com>
2022-11-27 19:41:32 +0545

    added more songs

--- ytm.js
+++ ytm.js
@@ -39,7 +39,7 @@ function onPlayerStateChange(event) {
 }
 
 function onPlayerReady(event) {
-  num = Math.floor(Math.random() * 7);
+  num = Math.floor(Math.random() * 12);
   setTimeout(() => {
     player.playVideoAt(num);
   }, 5);
