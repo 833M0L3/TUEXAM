c080131daa230d480ded2eed51cd1e276e47715b
833M0L3 <bimaldhital@gmail.com>
2024-12-01 15:57:18 +0545

    7th sem schedule update

--- scripts/index.js
+++ scripts/index.js
@@ -23,12 +23,12 @@ let currentsemname = getCookie('currentsemname');
 let dateLang = getCookie('datelang');
 
 if (currentsem === null) {
-  currentsem = '3';
+  currentsem = '6';
   setCookie('currentsem', currentsem, 365);
 }
 
 if (currentsemname === null) {
-  currentsemname = '4th';
+  currentsemname = '7th';
   setCookie('currentsemname', currentsemname, 365);
 }
 
